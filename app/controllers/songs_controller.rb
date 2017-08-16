28312297d53aaaef8280c4dfa6de188c5d96bd6e
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-08-16 16:36:21 -0400

    updated app to only render json

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,10 +2,11 @@ class SongsController < ApplicationController
 
   def index
     @songs = Song.all
-    respond_to do |format|
-      format.html {render :index}
-      format.json {render json: @songs}
-    end
+    # respond_to do |format|
+    #   format.html {render :index}
+    #   format.json {render json: @songs}
+    # end
+    render json: @songs
   end
 
 end
